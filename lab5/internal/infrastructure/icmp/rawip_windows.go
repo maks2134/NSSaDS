b69412868886ce68a9f71148cc024cf9a3496f2c
maks2134 <maks210306@yandex.by>
2026-09-14 23:42:28 +0300

    lab 5-6

--- lab5/internal/infrastructure/icmp/rawip_windows.go
+++ lab5/internal/infrastructure/icmp/rawip_windows.go
@@ -3,6 +3,7 @@
 package icmp
 
 import (
+	"errors"
 	"fmt"
 	"net"
 
@@ -14,19 +15,36 @@ type RawIP struct {
 }
 
 func OpenRawIP() (*RawIP, error) {
-	fd, err := windows.Socket(windows.AF_INET, windows.SOCK_RAW, windows.IPPROTO_IP)
+	protos := []int{windows.IPPROTO_ICMP, windows.IPPROTO_IP}
+	var last error
+	for _, proto := range protos {
+		r, err := openRawIPProto(proto)
+		if err == nil {
+			return r, nil
+		}
+		last = err
+	}
+	return nil, denyHint(fmt.Errorf("open raw ip socket: %w", last))
+}
+
+func openRawIPProto(proto int) (*RawIP, error) {
+	fd, err := windows.Socket(windows.AF_INET, windows.SOCK_RAW, proto)
 	if err != nil {
-		return nil, fmt.Errorf("open raw ip socket: %w", err)
+		return nil, err
 	}
 	if err := windows.SetsockoptInt(fd, windows.IPPROTO_IP, windows.IP_HDRINCL, 1); err != nil {
 		_ = windows.Closesocket(fd)
-		return nil, fmt.Errorf("set ip_hdrincl: %w", err)
+		return nil, err
 	}
-	if err := windows.SetsockoptInt(fd, windows.SOL_SOCKET, windows.SO_BROADCAST, 1); err != nil {
-		_ = windows.Closesocket(fd)
-		return nil, fmt.Errorf("set so_broadcast: %w", err)
-	}
+	_ = windows.SetsockoptInt(fd, windows.SOL_SOCKET, windows.SO_BROADCAST, 1)
 	return &RawIP{fd: fd}, nil
+}
+
+func denyHint(err error) error {
+	if errors.Is(err, windows.WSAEACCES) || errors.Is(err, windows.ERROR_ACCESS_DENIED) {
+		return fmt.Errorf("%w; run cmd.exe as Administrator", err)
+	}
+	return err
 }
 
 func (r *RawIP) SendIP(dst net.IP, packet []byte) error {
@@ -36,7 +54,7 @@ func (r *RawIP) SendIP(dst net.IP, packet []byte) error {
 	}
 	sa := &windows.SockaddrInet4{Addr: addr}
 	if err := windows.Sendto(r.fd, packet, 0, sa); err != nil {
-		return fmt.Errorf("raw ip send: %w", err)
+		return denyHint(fmt.Errorf("raw ip send: %w", err))
 	}
 	return nil
 }
